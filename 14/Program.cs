e3eb082aa9764a9a83db9cf1aa519b9661925934
Nikolayz <zans55@yandex.ru>
2023-02-04 18:23:33 +0300

    exercise 14

---
+++ 14/Program.cs
@@ -0,0 +1,16 @@
+﻿/*С клавиатуры вводятся два числа a и b. Выяснить, кратно ли число a числу b,
+ если нет, вывести остаток от деления a на b.*/
+double a,b;
+double c;
+a=Convert.ToInt32(System.Console.ReadLine());
+b=Convert.ToInt32(System.Console.ReadLine());
+bool odd=b%a==0;
+if (odd==true)
+{
+    System.Console.WriteLine($"{a} кратно {b}");
+}
+else
+{
+    c=b%a;
+    System.Console.WriteLine($" {c}");
+}
