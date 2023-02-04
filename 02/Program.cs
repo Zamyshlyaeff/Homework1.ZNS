5cdb635c126fa75394e3c6e846c6391516d6ba00
Nikolayz <zans55@yandex.ru>
2023-02-04 14:50:27 +0300

    exercise 2

---
+++ 02/Program.cs
@@ -0,0 +1,21 @@
+﻿/*  С клавиатуры вводятся два вещественных числа. 
+Проверять является ли одно из них квадратом второго*/
+
+double a,b;
+
+a=Convert.ToDouble(Console.ReadLine());
+b=Convert.ToDouble(Console.ReadLine());
+if (b==a*a)
+{
+    System.Console.WriteLine("$ Число {b} является квадратом числа {a}");
+}
+else if  (a==b*b)
+{ 
+    
+    System.Console.WriteLine("$ Число {a} является квадратом числа {b}");
+        }
+ else 
+ {
+    System.Console.WriteLine(" Ни одна из чисел не является квадратом другой числа ");
+
+ }
