e37362bc8e5fc4212aac59305b8948b860a440ff
Nikolayz <zans55@yandex.ru>
2023-02-04 15:14:25 +0300

    exercise 4

---
+++ 04/Program.cs
@@ -0,0 +1,31 @@
+﻿// По заданному с клавиатуры номеру дня недели вывести его название
+int a; // Ещё понял что можно сделать через массив, просто технически не знаю комманд
+a=Convert.ToInt32(System.Console.ReadLine());
+if (a==1)
+{
+System.Console.WriteLine("Сегодня понедельник!");
+}
+if (a==2)
+{
+System.Console.WriteLine("Сегодня вторник!");
+}
+if (a==3)
+{
+System.Console.WriteLine("Сегодня среда!");
+}
+if (a==4)
+{
+System.Console.WriteLine("Сегодня четверг!");
+}
+if (a==5)
+{
+System.Console.WriteLine("Ура пятница!");
+}
+if (a==6)
+{
+System.Console.WriteLine("Дожили до субботы!");
+}
+if (a==7)
+{
+System.Console.WriteLine("Уже воскресенье!");
+}
