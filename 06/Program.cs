889136cbe39c8032a3868a8d48017da49727ad2d
Nikolayz <zans55@yandex.ru>
2023-02-04 15:45:29 +0300

    exercise 6

---
+++ 06/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления значения функции y = sin(a). (Класс Math). 
+Console.Write("x = ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("E = ");
+            double E = double.Parse(Console.ReadLine());
+            double u = x, s = x;
+            int n = 1;
+           while (Math.Abs(u) > E)
+            {
+                s += u *= -x * x / ++n / ++n;
+            }
+            Console.WriteLine(@$"     sin({x}) = {s}, 
+Math.Sin({x}) = {Math.Sin(x)}");
+            Console.ReadLine();
+            
