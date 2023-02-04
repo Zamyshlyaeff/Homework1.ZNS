49dc58fb5aa4c5f234a30daee932ebf75b2dc23f
Nikolayz <zans55@yandex.ru>
2023-02-04 15:56:03 +0300

    exercise 7

---
+++ 07/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным.
+int a;
+a=Convert.ToInt32(System.Console.ReadLine());
+bool odd=a%2==0;
+if (odd==true)
+{
+System.Console.WriteLine($"Число {a} является чётным");
+}
+else
+{
+    System.Console.WriteLine($"Число {a} является нечётным");
+}
