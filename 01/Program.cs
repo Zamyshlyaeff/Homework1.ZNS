4212f9d6c26c9d5e830ebeeb3185eb137d41e7d3
Nikolayz <zans55@yandex.ru>
2023-02-04 14:09:43 +0300

    exercise 1

---
+++ 01/Program.cs
@@ -0,0 +1,8 @@
+﻿// С клавиатуры вводится целое число. Вывести квадрат числа
+int a,b;
+a=Convert.ToInt32(Console.ReadLine());
+b=a*a;
+System.Console.WriteLine($"Квадрат числа {a} найден и равен {b}");
+
+
+
