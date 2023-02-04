654695cb8aaf22124c6d056e44d805664c1c07a7
Nikolayz <zans55@yandex.ru>
2023-02-04 16:53:05 +0300

    exercise 10

---
+++ 10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
+int a;
+int b;
+int c;
+a=Convert.ToInt32(System.Console.ReadLine());
+b=(a/10);
+c=a-(b*10);
+System.Console.WriteLine($"Последняя цифра числа {a} это {c}");
+
