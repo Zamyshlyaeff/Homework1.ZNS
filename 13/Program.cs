df415c80fc41ee1843a4f925386ffc461321fc05
Nikolayz <zans55@yandex.ru>
2023-02-04 18:01:05 +0300

    exercise 13

---
+++ 13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Удалить вторую цифру целого числа введенного с клавиатуры.
+int a,b,c,d;
+a=Convert.ToInt32(System.Console.ReadLine());
+b=a/10;
+c=b-b/10;
+if (a>100)
+{
+d=a-c*10;
+System.Console.WriteLine($"{d}");
+}
+else
+{
+    d=a/10;
+    System.Console.WriteLine($"{d}");
+}
