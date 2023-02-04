ece7e3684b52121e7d9ff52a2d1937c146c1ddf6
Nikolayz <zans55@yandex.ru>
2023-02-04 18:29:09 +0300

    exercise 15

---
+++ 15/Program.cs
@@ -0,0 +1,14 @@
+﻿/* С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, 
+что её нет (Вывести: NO)*/
+int a;
+a=Convert.ToInt32(System.Console.ReadLine());
+if (a>=100)
+{
+    a=a-a/10*10;
+    System.Console.WriteLine($"{a}");
+
+}
+else 
+{
+    System.Console.WriteLine("NO");
+}
