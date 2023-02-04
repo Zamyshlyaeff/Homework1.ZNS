6a5484bc3f2a0f3c50840b0644cdeb337daeee68
Nikolayz <zans55@yandex.ru>
2023-02-04 17:42:12 +0300

    exercise 12

---
+++ 12/Program.cs
@@ -0,0 +1,18 @@
+﻿/* С клавиатуры вводится целое число из  диапазона  [10, 99]. 
+Показать наибольшую цифру числа*/
+int a,b,c;
+a=Convert.ToInt32(System.Console.ReadLine());
+b=a/10;
+c=a-(b*10);
+if (b>c)
+{
+    System.Console.WriteLine($"Число {b} больше чем {c}");
+}
+if (b<c)
+{
+    System.Console.WriteLine($"Число {b} меньше чем {c}");
+}
+if (b==c)
+{
+    System.Console.WriteLine($"Числа {b} и {c} равны");
+}
