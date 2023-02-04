020622974f878c8a7fe6248d87f12b7dda171231
Nikolayz <zans55@yandex.ru>
2023-02-04 17:32:39 +0300

    exercise 11

---
+++ 11/Program.cs
@@ -0,0 +1,9 @@
+﻿// Дано число больше 9. Вывести на экран  вторую цифру числа с конца
+int a = new Random().Next(9,1000);
+int b,c;
+System.Console.WriteLine($"{a}");
+b=a/10;
+c=b-(b/10)*10;
+
+System.Console.WriteLine($"Вторая цифра с конца это {c}");
+
